0d724c90a3c0bcbe5561744543cb9cb27b1fb529
kopejkinaviktoria3@gmail.com <kopejkinaviktoria3@gmail.com>
2024-03-19 03:50:32 +0300

    fixup! fix

--- src/main/java/sunposition/springday/repository/InMemoryDayDAO.java
+++ src/main/java/sunposition/springday/repository/InMemoryDayDAO.java
@@ -2,7 +2,6 @@ package sunposition.springday.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import sunposition.springday.model.Day;
 
 import java.time.LocalDate;
