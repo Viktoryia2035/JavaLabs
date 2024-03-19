aa7a99e842234e42797dbfafcab7da8893de6265
kopejkinaviktoria3@gmail.com <kopejkinaviktoria3@gmail.com>
2024-03-19 03:41:10 +0300

    fixup! fix

--- src/main/java/sunposition/springday/cache/InMemoryCache.java
+++ src/main/java/sunposition/springday/cache/InMemoryCache.java
@@ -3,42 +3,39 @@ package sunposition.springday.cache;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
-public class InMemoryCache {
+public class InMemoryCache<K, V> {
 
     private static final int MAX_SIZE = 100;
-    private final Map<String, Object> cacheMap = new HashMap<>();
+    private final Map<K, V> cacheMap = new HashMap<>();
 
-    public void put(String key, Object value) {
+    public void put(K key, V value) {
         if (key != null && value != null) {
-            if (cacheMap.containsKey(key)) {
-                cacheMap.put(key, value);
-            } else {
-                cacheMap.put(key, value);
-                if (cacheMap.size() >= MAX_SIZE) {
-                    cacheMap.clear();
-                }
+            cacheMap.put(key, value);
+            if (cacheMap.size() >= MAX_SIZE) {
+                cacheMap.clear();
             }
         }
     }
 
-    public Object get(String key) {
+    public V get(K key) {
         if (key != null) {
             return cacheMap.get(key);
         }
         return null;
     }
 
-    public boolean containsKey(String key) {
+    public boolean containsKey(K key) {
         if (key != null) {
             return cacheMap.containsKey(key);
         }
         return false;
     }
 
-    public void remove(String key) {
+    public void remove(K key) {
         if (key != null) {
             cacheMap.remove(key);
         }
@@ -46,5 +43,9 @@ public class InMemoryCache {
 
     public void clear() {
         cacheMap.clear();
+    }
+
+    public Iterable<Map.Entry<K, V>> getNativeCache() {
+        return cacheMap.entrySet();
     }
 }
--- src/main/java/sunposition/springday/repository/InMemoryDayDAO.java
+++ src/main/java/sunposition/springday/repository/InMemoryDayDAO.java
@@ -2,6 +2,7 @@ package sunposition.springday.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sunposition.springday.model.Day;
 
 import java.time.LocalDate;
@@ -18,8 +19,8 @@ public interface InMemoryDayDAO extends JpaRepository<Day, Long> {
     Day findByDateOfSunriseSunset(LocalDate dateOfSunriseSunset);
 
     @Query("SELECT d FROM Day d JOIN d.country c WHERE c.name = :countryName AND d.weatherConditions = :weatherConditions")
-    List<Day> findByCountryNameAndWeatherConditions(String countryName, String weatherConditions);
+    List<Day> findByCountryNameAndWeatherConditions(@Param("name") String countryName, @Param("wea_condition") String weatherConditions);
 
     @Query("SELECT d FROM Day d JOIN d.country c WHERE c.capital = :capital AND d.timeOfSunrise = :timeOfSunrise")
-    List<Day> findByCapitalAndTimeOfSunrise(String capital, LocalTime timeOfSunrise);
+    List<Day> findByCapitalAndTimeOfSunrise(@Param("capital") String capital, @Param("sunrise") LocalTime timeOfSunrise);
 }
